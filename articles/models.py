48c6020e45fa8905725ec6e0e95134f630b7d8f9
minwater96 <rlaalstn2568@naver.com>
2024-08-01 11:58:50 +0900

    comment read 기능구현

--- articles/models.py
+++ articles/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-
+    # comment_set =  => 자동생성
 
 class Comment(models.Model):
     content = models.TextField()
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE) #ForeigKey는 자동으로 부모-자식간 연결할 수 있는 다리를 만들어줌
+    # article_id =  => 자동생성
