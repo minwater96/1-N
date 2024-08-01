cd6fb7a00fbf872f939314c495180812ab1ad7cd
minwater96 <rlaalstn2568@naver.com>
2024-08-01 14:05:45 +0900

    commit update

--- articles/forms.py
+++ articles/forms.py
@@ -1,7 +1,17 @@
 from django import forms
-from .models import Article
+from .models import Article, Comment
 
 class ArticleForm(forms.ModelForm):
     class Meta():
         model = Article
         fields = '__all__'
+
+class CommentForm(forms.ModelForm):
+    class Meta():
+        model = Comment
+
+        #fields => 추가할 필드 이름 목록 10개중 1개 받을때
+        fields = ('content', )
+
+        # exclude => 제거할 필드 이름 목록 10개중 9기
+        # fields = ('content', )
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Article
-from .forms import ArticleForm
+from .forms import ArticleForm, CommentForm
 
 
 # Create your views here.
@@ -15,9 +15,11 @@ def index(request):
 
 def detail(request, id):
     article = Article.objects.get(id=id)
+    form = CommentForm()
 
     context = {
         'article': article,
+        'form': form
     }
 
     return render(request, 'detail.html', context)
@@ -27,7 +29,7 @@ def create(request):
         form = ArticleForm(request.POST)
         if form.is_valid():
             article = form.save()
-            return redirect('articles:detail', id=article.id)
+            return redirect('articles:detail', id=article_id)
     else:
         form = ArticleForm()
 
@@ -36,3 +38,23 @@ def create(request):
     }
 
     return render(request, 'form.html', context)
+
+def comment_create(request, article_id):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+
+            # 1. 객체 저장 방법
+            # article = Article.objects.get(id=article_id)
+            # comment.article = article
+            # comment.save()
+
+            # 2. integer(숫자)를 저장하는 방법
+            comment.article_id = article_id
+            comment.save()
+
+            return redirect('articles:detail', id=article_id)
+    else:
+        return redirect('articles:index')
+
